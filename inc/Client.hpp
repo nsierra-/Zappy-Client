f7c811d40864b46f780b2a27b14c2b36b21a9142
nsierra- <nsierra-@student.42.fr>
2015-05-29 01:52:19 +0200

    protos

--- inc/Client.hpp
+++ inc/Client.hpp
@@ -2,7 +2,7 @@
 //             .'         `.
 //            :             :        File       : Client.hpp
 //           :               :       Creation   : 2015-05-21 00:43:58
-//           :      _/|      :       Last Edit  : 2015-05-23 03:26:09
+//           :      _/|      :       Last Edit  : 2015-05-29 01:43:18
 //            :   =/_/      :        Author     : nsierra-
 //             `._/ |     .'         Mail       : nsierra-@student.42.fr
 //          (   /  ,|...-'
@@ -15,6 +15,7 @@
 # define CLIENT_HPP
 
 # include <string>
+# include <map>
 # include <regex>
 # include <fstream>
 # include "Network.hpp"
@@ -32,18 +33,39 @@ class	Client
 	bool					loop(void);
 
   private:
-  	static const std::regex	_serverInfosFormat;
-	const std::string		_teamName;
-	Network					*_network;
-	unsigned int			_availableConnections;
-	unsigned int			_mapX;
-	unsigned int			_mapY;
-	std::ofstream 			_ofs;
-	// unsigned int			_timeRemaining;
+
+  	enum eDirection : int {
+  		UP,
+  		LEFT,
+  		DOWN,
+  		RIGHT
+  	};
+
+  	static const std::regex			_serverInfosFormat;
+	const std::string				_teamName;
+	Network							*_network;
+	unsigned int					_availableConnections;
+	unsigned int					_mapX;
+	unsigned int					_mapY;
+	std::ofstream 					_ofs;
+	std::map<std::string, size_t>	_inventory;
 
 	std::string				_sendTeamInfo(void);
 	void					_loadServerInfos(const std::string &);
 	void					_forkstem(void);
+
+	/*
+	** Actions
+	*/
+	void					_move(eDirection);
+	void					_see(void);
+	void					_updateInventory(void);
+	void					_take(const std::string &);
+	void					_drop(const std::string &);
+	void					_expulse(void);
+	void					_broadcast(const std::string &);
+	void					_spell(void);
+	void					_egg(void);
 };
 
 #endif /* CLIENT_HPP */
