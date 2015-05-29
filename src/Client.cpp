c8a16daeece8f1d9d436aa310ceaa8f8e50a0530
nsierra- <nsierra-@student.42.fr>
2015-05-29 17:11:20 +0200

    actions

--- src/Client.cpp
+++ src/Client.cpp
@@ -2,7 +2,7 @@
 //             .'         `.
 //            :             :        File       : Client.cpp
 //           :               :       Creation   : 2015-05-21 00:44:59
-//           :      _/|      :       Last Edit  : 2015-05-23 03:55:33
+//           :      _/|      :       Last Edit  : 2015-05-29 17:10:35
 //            :   =/_/      :        Author     : nsierra-
 //             `._/ |     .'         Mail       : nsierra-@student.42.fr
 //          (   /  ,|...-'
@@ -22,13 +22,26 @@
 
 const std::regex	Client::_serverInfosFormat("(\\d+)\\n(\\d+) (\\d+)\\n");
 
+std::map<Client::eDirection, std::string>	Client::_directionMap = 
+	{
+		{ UP, "avance\n" },
+		{ TURN_LEFT, "gauche\n" },
+		{ TURN_RIGHT, "droite\n" }
+	}
+;
+
+
 Client::Client(unsigned int port, std::string teamName, std::string hostName) :
-_teamName(teamName),
-_network(new Network(port, hostName))
+	_teamName(teamName),
+	_network(new Network(port, hostName)),
+	_level(1)
 {
+
 	std::stringstream name;
 	name << "debug/" << getpid();
 	_ofs.open(name.str().c_str());
+
+	_inventory["nourriture"] = 10;
 }
 
 Client::Client(Client const & src) :
@@ -50,6 +63,7 @@ Client	&Client::operator=(Client const & rhs)
 	return *this;
 }
 
+
 bool	Client::loop(void)
 {
 	std::string msg;
@@ -63,11 +77,17 @@ bool	Client::loop(void)
 			_forkstem();
 		else 
 		{
-			msg = _network->recieve();	
+			_ia();
+			msg = _network->recieve();
 			_ofs << getpid() << " recoit " << msg << std::endl;
 		}
 	}
 	return true;
+}
+
+void	Client::_ia(void)
+{
+
 }
 
 void			Client::_forkstem(void)
@@ -92,6 +112,100 @@ void			Client::_forkstem(void)
 		_ofs << E_FORK << std::endl;
 		exit(EXIT_FAILURE);
 	}
+}
+
+void					Client::_move(enum eDirection dir)
+{
+	_ofs << _directionMap[dir] << std::endl;
+	_ofs << _network->send(_directionMap[dir]) << std::endl;
+}
+
+void					Client::_see(void)
+{
+	std::string			data;
+
+	_ofs << "voir" << std::endl;
+	data = _network->send("voir\n");
+	_ofs << data << std::endl;
+}
+
+void					Client::_updateInventory(const std::string &obj, int qty)
+{
+	_inventory[obj] += qty;
+}
+
+void					Client::_updateInventory(void)
+{
+	std::string			data;
+
+	_ofs << "inventaire" << std::endl;
+	data = _network->send("inventaire\n");
+	_ofs << data << std::endl;
+}
+
+void					Client::_take(const std::string & obj)
+{
+	std::string			message = "prendre ";
+	std::string			data;
+
+	message += obj + "\n";
+	_ofs << message << std::endl;
+	data = _network->send(message);
+	_ofs << data << std::endl;
+
+	if (data == "ok\n")
+		_updateInventory(obj, 1);
+}
+
+void					Client::_drop(const std::string & obj)
+{
+	std::string			message = "prendre ";
+	std::string			data;
+
+	message += obj + "\n";
+	_ofs << message << std::endl;
+	data = _network->send(message);
+	_ofs << data << std::endl;
+
+	if (data == "ok\n")
+		_updateInventory(obj, -1);
+}
+
+void					Client::_expulse(void)
+{
+	std::string			data;
+
+	_ofs << "expulse" << std::endl;
+	data = _network->send("expulse\n");
+	_ofs << data << std::endl;
+}
+
+void					Client::_broadcast(const std::string & msg)
+{
+	_ofs << msg << std::endl;
+	_ofs << _network->send(msg) << std::endl;
+}
+
+void					Client::_incantation(void)
+{
+	std::string			data;
+
+	_ofs << "incantation" << std::endl;
+	data = _network->send("incantation\n");
+	_ofs << data << std::endl;
+
+	if (data == "elevation en cours\n")
+	{
+		data = _network->recieve();
+		_ofs << data << std::endl;
+		_level++;
+	}
+}
+
+void					Client::_egg(void)
+{
+	_ofs << "fork" << std::endl;
+	_ofs << _network->send("fork\n") << std::endl;	
 }
 
 void			Client::_loadServerInfos(const std::string &infos)
